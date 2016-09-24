596ae475c196036fcf972cf4f6de6ec766502ee4
oliver <o.guggenbuehl@gmail.com>
2016-09-24 15:03:43 +0200

    Update flask-example.py

--- flask/flask-example.py
+++ flask/flask-example.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+
 @app.route('/')
 def hello_fedora():
-    return 'Hello from Fedora!'
+    return 'Hello from Fedora! NS: {0}'.format(os.getenv('OPENSHIFT_BUILD_NAMESPACE'))
 
 if __name__ == '__main__':
     app.run("0.0.0.0")
